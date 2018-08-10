7051757dedf5a2c9989144cb28f32dade67387d0
Robert Raposa <rraposa@edx.org>
2018-08-10 16:15:06 -0400

    Split out TieredCacheMiddleware from RequestCacheMiddleware.

--- edx_django_utils/__init__.py
+++ edx_django_utils/__init__.py
@@ -4,6 +4,6 @@ EdX utilities for Django Application development..
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 default_app_config = 'edx_django_utils.apps.EdxDjangoUtilsConfig'  # pylint: disable=invalid-name
--- edx_django_utils/cache/tests/test_middleware.py
+++ edx_django_utils/cache/tests/test_middleware.py
@@ -22,7 +22,6 @@ class TestRequestCacheMiddleware(TestCase):
         super(TestRequestCacheMiddleware, self).setUp()
         self.middleware = middleware.RequestCacheMiddleware()
         self.request = RequestFactory().get('/')
-        self.request.user = self._mock_user(is_staff=True)
 
         self.request_cache = RequestCache()
         self.other_request_cache = RequestCache(TEST_NAMESPACE)
@@ -32,25 +31,6 @@ class TestRequestCacheMiddleware(TestCase):
         self.middleware.process_request(self.request)
 
         self._check_request_caches_cleared()
-        self.assertFalse(self.request_cache.get_cached_response(SHOULD_FORCE_CACHE_MISS_KEY).value)
-
-    def test_process_request_force_django_cache_miss(self):
-        request = RequestFactory().get('/?{}=tRuE'.format(FORCE_CACHE_MISS_PARAM))
-        request.user = self._mock_user(is_staff=True)
-
-        self.middleware.process_request(request)
-
-        self._check_request_caches_cleared()
-        self.assertTrue(self.request_cache.get_cached_response(SHOULD_FORCE_CACHE_MISS_KEY).value)
-
-    def test_process_request_force_django_cache_miss_non_staff(self):
-        request = RequestFactory().get('/?{}=tRuE'.format(FORCE_CACHE_MISS_PARAM))
-        request.user = self._mock_user(is_staff=False)
-
-        self.middleware.process_request(request)
-
-        self._check_request_caches_cleared()
-        self.assertFalse(self.request_cache.get_cached_response(SHOULD_FORCE_CACHE_MISS_KEY).value)
 
     def test_process_response(self):
         response = self.middleware.process_response(self.request, EXPECTED_VALUE)
@@ -73,6 +53,39 @@ class TestRequestCacheMiddleware(TestCase):
         """ Dirties the request caches to ensure the middleware is clearing it. """
         self.request_cache.set(TEST_KEY, EXPECTED_VALUE)
         self.other_request_cache.set(TEST_KEY, EXPECTED_VALUE)
+
+
+class TestTieredCacheMiddleware(TestCase):
+
+    def setUp(self):
+        super(TestTieredCacheMiddleware, self).setUp()
+        self.middleware = middleware.TieredCacheMiddleware()
+        self.request = RequestFactory().get('/')
+        self.request.user = self._mock_user(is_staff=True)
+
+        self.request_cache = RequestCache()
+        self.request_cache.clear_all_namespaces()
+
+    def test_process_request(self):
+        self.middleware.process_request(self.request)
+
+        self.assertFalse(self.request_cache.get_cached_response(SHOULD_FORCE_CACHE_MISS_KEY).value)
+
+    def test_process_request_force_django_cache_miss(self):
+        request = RequestFactory().get('/?{}=tRuE'.format(FORCE_CACHE_MISS_PARAM))
+        request.user = self._mock_user(is_staff=True)
+
+        self.middleware.process_request(request)
+
+        self.assertTrue(self.request_cache.get_cached_response(SHOULD_FORCE_CACHE_MISS_KEY).value)
+
+    def test_process_request_force_django_cache_miss_non_staff(self):
+        request = RequestFactory().get('/?{}=tRuE'.format(FORCE_CACHE_MISS_PARAM))
+        request.user = self._mock_user(is_staff=False)
+
+        self.middleware.process_request(request)
+
+        self.assertFalse(self.request_cache.get_cached_response(SHOULD_FORCE_CACHE_MISS_KEY).value)
 
     def _mock_user(self, is_staff=True):
         mock_user = MagicMock()
