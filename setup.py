13858f34291975b22d167b99bc5df1aeaec6863c
DawoudSheraz <dawoud.sheraz@arbisoft.com>
2019-06-11 11:23:04 +0500

    add constraints check

--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ def is_requirement(line):
         line.startswith('-r') or
         line.startswith('#') or
         line.startswith('-e') or
-        line.startswith('git+')
+        line.startswith('git+') or
+        line.startswith('-c')
     )
 
 
