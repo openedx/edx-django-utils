387fa1b250ea480215a84e2d56be5c960b650e94
Calen Pennington <cale@edx.org>
2020-04-07 09:48:39 -0400

    Unpin psutils

--- edx_django_utils/__init__.py
+++ edx_django_utils/__init__.py
@@ -4,7 +4,7 @@ EdX utilities for Django Application development..
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "3.1"
+__version__ = "3.1.1"
 
 default_app_config = (
     "edx_django_utils.apps.EdxDjangoUtilsConfig"
