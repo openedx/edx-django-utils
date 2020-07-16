c11dfc4cb51d7c696d4b023fe1c44f9ae1bbfac1
Robert Raposa <rraposa@edx.org>
2020-07-16 17:14:44 -0400

    Try try again.

--- edx_django_utils/monitoring/code_owner/tests/test_utils.py
+++ edx_django_utils/monitoring/code_owner/tests/test_utils.py
@@ -105,5 +105,5 @@ class MonitoringUtilsTests(TestCase):
                 average_time = time / call_iterations
                 self.assertLess(average_time, 0.0005, 'Mapping takes {}s which is too slow.'.format(average_time))
 
-# Disable to keep pycodestyle happy.
-# noqa
+
+# Make pycodestyle happy with 2 blank lines.
