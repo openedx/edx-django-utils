3b4299e1318243a4521ae89a0828552aed9fcc3b
Robert Raposa <rraposa@edx.org>
2020-07-16 16:48:29 -0400

    arggghhh

--- edx_django_utils/monitoring/code_owner/tests/test_utils.py
+++ edx_django_utils/monitoring/code_owner/tests/test_utils.py
@@ -104,4 +104,3 @@ class MonitoringUtilsTests(TestCase):
                 )
                 average_time = time / call_iterations
                 self.assertLess(average_time, 0.0005, 'Mapping takes {}s which is too slow.'.format(average_time))
-
