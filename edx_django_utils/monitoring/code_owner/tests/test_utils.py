18a18b450c58d12b5e4c5c96699900468eae0968
Robert Raposa <rraposa@edx.org>
2020-07-16 16:45:11 -0400

    fixed pointless pycodestyle error

--- edx_django_utils/monitoring/code_owner/tests/test_utils.py
+++ edx_django_utils/monitoring/code_owner/tests/test_utils.py
@@ -104,3 +104,4 @@ class MonitoringUtilsTests(TestCase):
                 )
                 average_time = time / call_iterations
                 self.assertLess(average_time, 0.0005, 'Mapping takes {}s which is too slow.'.format(average_time))
+
