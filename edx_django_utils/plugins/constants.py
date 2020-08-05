86bb72325b72e5d1d5424f32d6ae8f4d5389d875
jinder1s <msingh@edx.org>
2020-08-05 14:27:17 -0400

    Removing edx-platform specific from this repo

--- edx_django_utils/plugins/constants.py
+++ edx_django_utils/plugins/constants.py
@@ -13,34 +13,6 @@ PLUGIN_APP_CLASS_ATTRIBUTE_NAME = "plugin_app"
 PLUGIN_APP_SETTINGS_FUNC_NAME = "plugin_settings"
 
 
-class ProjectType():
-    """
-    The ProjectType enum defines the possible values for the Django Projects
-    that are available in the edx-platform. Plugin apps use these values to
-    declare explicitly which projects they are extending.
-    """
-
-    LMS = "lms.djangoapp"
-    CMS = "cms.djangoapp"
-
-
-class SettingsType():
-    """
-    The SettingsType enum defines the possible values for the settings files
-    that are available for extension in the edx-platform. Plugin apps use these
-    values (in addition to ProjectType) to declare explicitly which settings
-    (in the specified project) they are extending.
-
-    See https://github.com/edx/edx-platform/master/lms/envs/docs/README.rst for
-    further information on each Settings Type.
-    """
-
-    PRODUCTION = "production"
-    COMMON = "common"
-    DEVSTACK = "devstack"
-    TEST = "test"
-
-
 class PluginSettings():
     """
     The PluginSettings enum defines dictionary field names (and defaults)
