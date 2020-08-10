74fbf4e43776c83ffdf10820da93e4232aec0643
jinder1s <msingh@edx.org>
2020-08-10 16:00:06 -0400

    Correcting import order

--- edx_django_utils/plugins/__init__.py
+++ edx_django_utils/plugins/__init__.py
@@ -4,10 +4,10 @@ Plugins infrastructure
 See README.rst for details.
 """
 
-from .constants import PluginSettings, PluginURLs, PluginSignals, PluginContexts
+from .constants import PluginContexts, PluginSettings, PluginSignals, PluginURLs
 from .plugin_apps import get_apps
 from .plugin_contexts import get_plugins_view_context
-from .plugin_manager import PluginManager, PluginError
+from .plugin_manager import PluginError, PluginManager
 from .plugin_settings import add_plugins
 from .plugin_signals import connect_receivers
 from .plugin_urls import get_patterns, get_url
