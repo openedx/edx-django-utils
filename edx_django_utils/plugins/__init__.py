4a470d59df75b0eebc82e065b569a607843346bb
jinder1s <msingh@edx.org>
2020-08-10 15:30:03 -0400

    Adding missing api point

--- edx_django_utils/plugins/__init__.py
+++ edx_django_utils/plugins/__init__.py
@@ -7,7 +7,7 @@ See README.rst for details.
 from .constants import PluginSettings, PluginURLs
 from .plugin_apps import get_apps
 from .plugin_contexts import get_plugins_view_context
-from .plugin_manager import PluginManager
+from .plugin_manager import PluginManager, PluginError
 from .plugin_settings import add_plugins
 from .plugin_signals import connect_receivers
 from .plugin_urls import get_patterns
