9b47f1dd60e847ce96885189dc801858e43abd58
Saad Ali <saad.ali@arbisoft.com>
2021-07-19 14:50:26 +0500

    Resolving quality tests E501

--- edx_django_utils/management/commands/manage_group.py
+++ edx_django_utils/management/commands/manage_group.py
@@ -30,7 +30,9 @@ class Command(BaseCommand):  # lint-amnesty, pylint: disable=missing-class-docst
             self.stderr.write(_('Did not find a group with name "{}" - skipping.').format(group_name))
 
     @transaction.atomic
-    def handle(self, group_name, is_remove, permissions=None, *args, **options):  # lint-amnesty, pylint: disable=arguments-differ, keyword-arg-before-vararg
+    def handle(  # lint-amnesty, pylint: disable=arguments-differ, keyword-arg-before-vararg
+        self, group_name, is_remove, permissions=None, *args, **options
+        ):
 
         if is_remove:
             self._handle_remove(group_name)
