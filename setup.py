87e2dbc280b6931cd34518c39d8872c278f4d259
Rebecca Graber <rgraber@edx.org>
2021-11-08 12:49:02 -0500

    fix: rm extraneous parens

--- setup.py
+++ setup.py
@@ -90,7 +90,6 @@ def is_requirement(line):
     # UPDATED VIA SEMGREP - if you need to remove/modify this method remove this line and add a comment specifying why
 
     return line and line.strip() and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
-    )
 
 
 VERSION = get_version('edx_django_utils', '__init__.py')
